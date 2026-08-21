cdf97d8b284089e1a476b6c66eb07140935991ce
StarHub190 <muhammadzidanpriadi843@gmail.com>
2026-08-21 17:50:37 +0700

    add: income types

--- packages/shared-types/src/transactions.ts
+++ packages/shared-types/src/transactions.ts
@@ -36,3 +36,31 @@ export interface Spend {
 }
 
 export type SpendResponse = ApiResponse<Spend>;
+
+// income
+
+export interface AddIncome {
+  type: 'income';
+  amount: number;
+  title: string;
+  category_id: string;
+  account_id: string;
+  date: string;
+  notes: string | null;
+}
+
+export interface Income {
+  id: string;
+  type: 'income';
+  title: string;
+  amount: number;
+  fee: number;
+  date: string;
+  notes: string | null;
+  category: CategoryTrans;
+  account: AccountTrans;
+  created_at: string;
+}
+
+export type IncomeResponse = ApiResponse<Income>;
+
