dbe14a3791871aba3716a1fcbf80c79344e4f302
StarHub190 <muhammadzidanpriadi843@gmail.com>
2026-08-23 21:39:59 +0700

    add: Button ui

--- apps/client/src/components/ui/Button.tsx
+++ apps/client/src/components/ui/Button.tsx
@@ -1,16 +1,25 @@
 // props def
 
+import type { ReactNode } from 'react';
+
 interface ButtonProps {
-  label: string;
-  isDisabled: boolean;
+  label?: string;
+  icon?: ReactNode;
+  className?: string;
+  isDisabled?: boolean;
+  onClick: () => void;
 }
 
-const Button = ({ label, isDisabled }: ButtonProps) => {
-    return (
-        <button disabled={isDisabled} className="border-3 border-white bg-red text-white">
-            {label}
-        </button>
-    )
+const Button = ({ label, isDisabled, className, icon, onClick }: ButtonProps) => {
+  return (
+    <button
+      disabled={isDisabled}
+      className={`border-3 border-white bg-red text-white ${className}`}
+      onClick={onClick}
+    >
+      {label ? label: icon}
+    </button>
+  );
 };
 
-export default Button
+export default Button;
